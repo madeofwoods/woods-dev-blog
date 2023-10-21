81f3233f2445eca73625fa5d6f16f3f92237cd35
Matt Woods <mattwoodsmaths@gmail.com>
2023-10-21 16:54:11 +0200

    one toggle idea

--- components/ThemeToggle/ThemeToggle.jsx
+++ components/ThemeToggle/ThemeToggle.jsx
@@ -8,7 +8,14 @@ const ThemeToggle = () => {
   const { theme, toggle } = useContext(ThemeContext);
   return (
     <div className={styles.themeToggle} onClick={toggle}>
-      THeM
+      <div className={`${styles.moonContainer} ${theme == "light" ? styles.moonLight : styles.moonDark}`}>
+        <div className={styles.moon}></div>
+      </div>
+      <div className={`${styles.sunContainer}  ${theme == "light" ? styles.sunLight : styles.sunDark}`}>
+        <div className={styles.sun}></div>
+      </div>
+
+      <div className={`${styles.sunset}  ${theme == "light" ? styles.sunRises : styles.sunSets}`}></div>
     </div>
   );
 };
