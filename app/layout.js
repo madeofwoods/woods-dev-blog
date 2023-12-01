a75d5c930bb83e4f6420d0bc5d7412fd223308c1
Matt Woods <mattwoodsmaths@gmail.com>
2023-12-01 13:30:31 +0100

    add viewport metatag

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,7 @@ export const metadata = {
   metadataBase: new URL("https://blog.madeofwoods.com/"),
   title: { default: "MADEOFWOODS", template: `%s | M A D E O F W O O D S` },
   description: "Explore the latest posts from my frontend dev blog.",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
 };
 
 export default function RootLayout({ children }) {
